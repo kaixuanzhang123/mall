0d3f38555261f3383892ee6f7c4a90e29f0e2be4
kaixuanzhang123 <zhangkaixuan@navinfo.com>
2020-12-07 23:43:47 +0800

    跳轉搜索服務

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
@@ -1,6 +1,8 @@
 package com.atguigu.gulimall.product.config;
 
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -9,13 +11,18 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import sun.misc.Cache;
 
 @Configuration
+@EnableCaching
+@EnableConfigurationProperties(CacheProperties.class)
 public class MyCacheConfig {
     @Bean
     public org.springframework.data.redis.cache.RedisCacheConfiguration redisCacheConfiguration(
             CacheProperties cacheProperties) {
+
         CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+
         org.springframework.data.redis.cache.RedisCacheConfiguration config = org.springframework.data.redis.cache.RedisCacheConfiguration
                 .defaultCacheConfig();
+
         config = config.serializeValuesWith(
                 RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
         if (redisProperties.getTimeToLive() != null) {
